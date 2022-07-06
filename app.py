2126c73d9a899d74b15925b62f300e283ffb323a
Vaibhav Kumar <vaibhav.kumar@knoldus.com>
2022-07-06 15:13:45 +0530

    integrated data of storage with Sheet

--- app.py
+++ app.py
@@ -17,34 +17,37 @@ def costdata():
     if request.method == 'POST':
         global row
         User = request.form['User']
+        Cloud = request.form['Cloud']
+        OS = request.form['OS']
         RAM = float(request.form['RAM'])
         vCPUs = int(request.form['vCPUs'])
-        OS = request.form['OS']
-        NodeStorage= int(request.form['NodeStorage'])
-        Cloud = request.form['Cloud']
-        Number_of_Nodes = int(request.form['No of nodes'])
+        Number_of_Nodes = int(request.form['No of Nodes'])
+        StorageType = request.form['Storage Type']
+        StorageUnitSize= int(request.form['Storage Size'])
+        Number_of_Storage_Units = int(request.form['Storage Unit Quantity'])        
         
         if request.form.get('submit_button') == 'submit':
             # send data to gsheet
-            data = [User,RAM,vCPUs,OS,NodeStorage,Cloud,Number_of_Nodes]
+            data = [User,Cloud,OS,RAM,vCPUs,Number_of_Nodes,StorageType,\
+                    StorageUnitSize,Number_of_Storage_Units]
             UserSheet = roi_sheet.worksheet('Dashboard 1')
             UserSheet.insert_row(data,row)
             row = row + 1
 
             if Cloud == "AWS":
-                node_cost = spreadsheet.aws_cost(RAM,vCPUs,OS,NodeStorage,Cloud,Number_of_Nodes)
+                node_cost = spreadsheet.aws_cost(RAM,vCPUs,OS,StorageUnitSize,Cloud,Number_of_Nodes)
             elif Cloud == "GCP":
-                node_cost = spreadsheet.gcp_cost(RAM,vCPUs,OS,NodeStorage,Cloud,Number_of_Nodes)
+                node_cost = spreadsheet.gcp_cost(RAM,vCPUs,OS,StorageUnitSize,Cloud,Number_of_Nodes)
             else:
-                node_cost = spreadsheet.azure_cost(RAM,vCPUs,OS,NodeStorage,Cloud,Number_of_Nodes)
+                node_cost = spreadsheet.azure_cost(RAM,vCPUs,OS,StorageUnitSize,Cloud,Number_of_Nodes)
 
             node_cost.index.name=None
             return render_template('index.html',title="Dashboard",tables=[node_cost.head().to_html(index = False)],titles = ['Cloud Cost for nearest Node Config'],Heading="{} Node Data".format(Cloud))
 
         elif request.form.get('submit_button')=="compare":
-            aws_node_cost = spreadsheet.aws_cost(RAM,vCPUs,OS,NodeStorage,Cloud,Number_of_Nodes)
-            gcp_node_cost = spreadsheet.gcp_cost(RAM,vCPUs,OS,NodeStorage,Cloud,Number_of_Nodes)
-            azure_node_cost = spreadsheet.azure_cost(RAM,vCPUs,OS,NodeStorage,Cloud,Number_of_Nodes)
+            aws_node_cost = spreadsheet.aws_cost(RAM,vCPUs,OS,StorageUnitSize,Cloud,Number_of_Nodes)
+            gcp_node_cost = spreadsheet.gcp_cost(RAM,vCPUs,OS,StorageUnitSize,Cloud,Number_of_Nodes)
+            azure_node_cost = spreadsheet.azure_cost(RAM,vCPUs,OS,StorageUnitSize,Cloud,Number_of_Nodes)
             combine_cost = spreadsheet.cost_data_combine(aws_node_cost,gcp_node_cost,azure_node_cost)
 
             labels= combine_cost['Machine or Service'].values.tolist()
